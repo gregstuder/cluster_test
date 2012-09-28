53075debfc86beb7114a95e62bdca8678935bf7a
Greg Studer <greg@10gen.com>
2012-09-28 18:40:50 -0400

    checkpoint

--- basics_ubuntu.sh
+++ basics_ubuntu.sh
@@ -12,8 +12,16 @@ apt-get update
 apt-get install opscode-keyring # permanent upgradeable keyring
 
 # Install chef
-echo "chef chef/chef_server_url string none"\
- | debconf-set-selections && sudo apt-get install chef -y
+echo "chef chef/chef_server_url string http://`curl http://instance-data/latest/meta-data/public-hostname`:4000"\
+ | debconf-set-selections && sudo apt-get install chef chef-server -y
+
+
+#
+#
+#
+#
+
+
 
 # Install ruby and gem dependencies
 apt-get install -y ruby ruby-dev libopenssl-ruby rdoc ri irb build-essential wget ssl-cert curl
@@ -28,3 +36,42 @@ ruby setup.rb --no-format-executable
 
 cd ../..
 rm -R ./ruby-tmp
+
+gem install chef --no-ri --no-rdoc
+
+# Setup and run chef-solo
+mkdir -p /etc/chef
+
+echo "\
+file_cache_path \"/tmp/chef-solo\"
+cookbook_path \"/tmp/chef-solo/cookbooks\"" | tee /etc/chef/solo.rb
+
+read -d '' CONFIG <<"EOF"
+{
+  "chef_server": {
+    "server_url": "http://localhost:4000"
+  },
+  "run_list": [ "recipe[chef-server::rubygems-install]" ]
+}
+EOF
+echo "$CONFIG" | tee chef.json
+
+chef-solo -c /etc/chef/solo.rb -j ~/chef.json -r http://s3.amazonaws.com/chef-solo/bootstrap-latest.tar.gz
+
+# Setup knife
+
+#mkdir -p ~/.chef
+#cp /etc/chef/validation.pem /etc/chef/webui.pem ~/.chef
+#chown -R $USER ~/.chef
+
+# Need to hit enter...
+knife configure -i
+
+# Install knife-ec2
+
+apt-get update
+apt-get install -y ruby1.8-dev ruby1.8 ri1.8 rdoc1.8 irb1.8
+apt-get install -y libreadline-ruby1.8 libruby1.8 libopenssl-ruby
+apt-get install -y libxslt-dev libxml2-dev
+
+gem install knife-ec2 --no-rdoc --no-ri
